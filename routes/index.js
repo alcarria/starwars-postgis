8c97fec9441e0b0acf7acefa1486f88ac746e0c3
alcarria <alcarria@gmail.com>
2023-01-15 20:29:41 +0100

    soporte PostGIS
    version 2.0 con soporte postGIS

--- routes/index.js
+++ routes/index.js
@@ -2,68 +2,88 @@ var express = require('express');
 var router = express.Router();
 var o2x = require('object-to-xml');
 
-var personajes = [
-  {
-    Nombre: 'Darth vader',
-    Fuerza: 100,
-    Faccion: 'Imperio'
-  }
-  ];
+const pgp = require('pg-promise')(/* options */)
+const db = pgp('postgres://postgres:postgres@localhost:5432/starwars')
 
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Starwars page', personajes });
+router.get('/', function (req, res, next) {
+  db.any('SELECT * FROM personajes ORDER BY id ASC')
+    .then((personajes) => {
+      console.log('DATA:', personajes);
+      res.render('index', { title: 'Starwars page', personajes });
+    })
+    .catch((error) => {
+      console.log('ERROR:', error);
+      next(error);
+    })
 });
 
-router.post('/', function(req, res, next) {
+router.post('/', function (req, res, next) {
+  db.none('INSERT INTO personajes(nombre, fuerza, faccion) VALUES($1, $2, $3)', [req.body.nombre, req.body.fuerza, req.body.faccion])
+    .then(() => {
+      // success;
+      res.redirect('/');
+    })
+    .catch(error => {
+      console.log('ERROR:', error);
+      next(error);
+    });
+});
 
-  var nuevoPersonaje = { "Nombre": req.body.nombre, "Fuerza": req.body.fuerza, 
-  "Faccion": req.body.faccion };
-   
-  personajes.push(nuevoPersonaje);
-
-  res.redirect('/');
- });
-
- router.get('/character/:ID', function(req, res, next) {	
+router.get('/character/:ID', function (req, res, next) {
   var characterID = req.params.ID;
-  if (characterID<=personajes.length && characterID>0){
-    res.render('personaje', { title: 'Character page', personaje: personajes[characterID-1] });
-  }
-  else {
-    // render the error page
-    var err = new Error('Character Not Found');
-    err.status = 404;
-    next(err);
-  }
-  });
+  db.one('SELECT * FROM personajes WHERE id = $1', [characterID])
+    .then((personaje) => {
+      console.log('DATA:', personaje);
+      res.render('personaje', { title: 'Character page', personaje });
+    })
+    .catch((error) => {
+      console.log('ERROR:', error);
+      next(error);
+    })
+});
 
-  router.post('/character/:ID', function(req, res, next) {	
-    var characterID = req.params.ID;
-    if (characterID<=personajes.length && characterID>0){
-      personajes.splice(characterID-1,1);
-      res.redirect('/');		
-    }
-    else {
-      // render the error page
-      var err = new Error('Character Not Found');
-      err.status = 404;
-      next(err);
-    }
-    });
+router.post('/character/:ID', function (req, res, next) {
+  var characterID = req.params.ID;
+  db.result('DELETE FROM personajes WHERE id = $1', [characterID])
+    .then((result) => {
+      console.log(result.rowCount); // print how many records were deleted; 
+      res.redirect('/');
+    })
+    .catch((error) => {
+      console.log('ERROR:', error);
+      next(error);
+    })
+});
 
-    router.get('/xml', function(req, res, next) {
+router.get('/xml', function (req, res, next) {
+  db.any('SELECT * FROM personajes ORDER BY id ASC')
+    .then((personajes) => {
+      console.log('DATA:', personajes)
       res.set('Content-Type', 'text/xml');
-    res.send(o2x({
-    '?xml version="1.0" encoding="utf-8"?' : null, personajes: {"personaje": personajes}}));
-      
-    });
+      res.send(o2x({
+        '?xml version="1.0" encoding="utf-8"?': null, personajes: { "personaje": personajes }
+      }));
+    })
+    .catch((error) => {
+      console.log('ERROR:', error)
+      next(error);
+    })
+});
 
-    router.get('/json', function(req, res, next) {      
-          res.json(personajes);
-  });
+router.get('/json', function (req, res, next) {
+  db.any('SELECT * FROM personajes ORDER BY id ASC')
+    .then((personajes) => {
+      console.log('DATA:', personajes);
+      res.json(personajes);
+    })
+    .catch((error) => {
+      console.log('ERROR:', error);
+      next(error);
+    })
+});
 
 
 module.exports = router;
